6566f5de3b2bf23fe58ad74a708e1c16e99ed57d
kychanbp <ust.johnchan@gmail.com>
2018-12-13 23:15:04 +0800

    added try and except to deal with empty df: no positions

--- myproject/getCurrentPortfolio.py
+++ myproject/getCurrentPortfolio.py
@@ -5,10 +5,13 @@ ib.connect('127.0.0.1', 4002, clientId=5)
 
 def getCurrentPotfolio(ib):
     df = pd.DataFrame(ib.portfolio())
-    df['Ticker'] = df['contract'].apply(lambda x: x.symbol)
-    df['Exchange'] = df['contract'].apply(lambda x: x.primaryExchange)
-    df.drop(columns = ['contract'], inplace=True)
-    df = df[['account','Ticker','Exchange','position','marketPrice','averageCost','unrealizedPNL','realizedPNL']]
+    try:
+        df['Ticker'] = df['contract'].apply(lambda x: x.symbol)
+        df['Exchange'] = df['contract'].apply(lambda x: x.primaryExchange)
+        df.drop(columns = ['contract'], inplace=True)
+        df = df[['account','Ticker','Exchange','position','marketPrice','averageCost','unrealizedPNL','realizedPNL']]
+    except:
+        df = pd.DataFrame(columns = ['account','Ticker','Exchange','position','marketPrice','averageCost','unrealizedPNL','realizedPNL'])
     return df
 
 df = getCurrentPotfolio(ib)
