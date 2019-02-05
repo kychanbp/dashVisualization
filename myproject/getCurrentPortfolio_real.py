13848846959a9f5889f3ba9dd25ae257064c90c7
kychanbp <ust.johnchan@gmail.com>
2019-02-05 12:44:29 +0800

    get portfolio for live account

---
+++ myproject/getCurrentPortfolio_real.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from ib_insync import *
+ib = IB()
+ib.connect('127.0.0.1', 4001, clientId=5) 
+
+def getCurrentPotfolio(ib):
+    df = pd.DataFrame(ib.portfolio())
+    try:
+        df['Ticker'] = df['contract'].apply(lambda x: x.symbol)
+        df['Exchange'] = df['contract'].apply(lambda x: x.primaryExchange)
+        df.drop(columns = ['contract'], inplace=True)
+        df = df[['account','Ticker','Exchange','position','marketPrice','averageCost','unrealizedPNL','realizedPNL']]
+    except:
+        df = pd.DataFrame(columns = ['account','Ticker','Exchange','position','marketPrice','averageCost','unrealizedPNL','realizedPNL'])
+    return df
+
+df = getCurrentPotfolio(ib)
+
+df.to_csv("currentPortfolio.csv", index=False)
